81d6290c1f6073ee39589cedb79826ca4c6bd585
Luis Pestana <lpestana@protonmail.com>
2020-02-27 15:50:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,11 +42,11 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   if hash[key]
     hash = {
-      key += 1
+      key => key + 1
     }
   else
     hash = {
-      key = 1
+      key => 1
     }
   end
 end
